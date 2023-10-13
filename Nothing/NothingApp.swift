270dc582e129b992a91639ea8fc252cf30e30c86
Aslınur Topcu <Asl.nurtopcu13@gmail.com>
2023-10-13 08:37:32 +0300

    Initial Commit

---
+++ Nothing/NothingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NothingApp.swift
+//  Nothing
+//
+//  Created by Aslınur Topcu on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NothingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
